18421c8345c2c3a2c029d909aaf0a3271f84a215
Praison Benjamin <praisonbenjamin@gmail.com>
2021-03-01 17:19:03 -0600

    implemented learning page

--- src/services/lang-service.js
+++ src/services/lang-service.js
@@ -14,6 +14,34 @@ const LangService = {
             : res.json()
             )
     },
+
+    getLangHead() {
+        return fetch(`${config.API_ENDPOINT}/language/head`, {
+            headers: {
+                'authorization': `Bearer ${TokenService.getAuthToken()}`,
+            },
+        })
+        .then(res =>
+            (!res.ok)
+            ? res.json().then(e => Promise.reject(e))
+            :res.json()
+            )
+    
+    },
+
+    makeGuess(guess) {
+        return fetch(`${config.API_ENDPOINT}/language/guess`, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${TokenService.getAuthToken()}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ guess })
+        }).then(res =>
+            !res.ok
+            ? res.json().then(e => Promise.reject(e))
+            :res.json)
+    }
 }
 
 export default LangService
